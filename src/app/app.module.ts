dfdc8b1098d261024db9198c03a21e0ec67e6a53
Ritwik101010 <ritwik16singh@gmail.com>
2022-06-26 09:12:44 +0530

    Learn about modules and make create a new component through CLI and learn about template URLs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServingComponent } from './serving/serving.component';
 
 @NgModule({
-  declarations: [AppComponent,ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
