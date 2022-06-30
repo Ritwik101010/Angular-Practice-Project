759e2659439f2059adaf22d809a3643b9712bdb9
Ritwik101010 <ritwik16singh@gmail.com>
2022-06-30 06:29:35 +0530

    Adding styling

--- src/app/serving/serving.component.ts
+++ src/app/serving/serving.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-serving',
+  selector: '.app-serving',
   template: `<h1>Heading 1</h1>
     ,
     <h2>Heading 2</h2>
